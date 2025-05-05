057607632b4ab6043f78a9f6d5485681e64f9905
nas3ts <nkisowile@gmail.com>
2025-05-05 15:17:43 +0300

    create views

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def todo_list(request):
+    # Handle GET request: retrieve and return all todo items
+    if request.method == 'GET':
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+
+    # Handle POST request: create a new todo item
+    elif request.method == 'POST':
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def todo_detail(request, pk):
+    # Try to retrieve the todo item by primary key (pk)
+    try:
+        todo = Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+        return Response({'error': 'Todo not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    # Handle GET request: return the specific todo item
+    if request.method == 'GET':
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data)
+
+    # Handle PUT request: update the todo item
+    elif request.method == 'PUT':
+        serializer = TodoSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # Handle DELETE request: delete the todo item
+    elif request.method == 'DELETE':
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
